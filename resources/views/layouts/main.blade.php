c4195ca7bf3dfbbe4eb8a4d1b411b72221a02a43
Matheus <Matheus@bc2.sede>
2021-11-09 17:55:03 -0300

    UPDATE 0.002a 09/11/2021
    Small progress update.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,107 +1,106 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PLACEHOLDER</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- Custom CSS --}}
-        <link rel="stylesheet" href="css/global.css">
-        @yield('header')
+    <title>PLACEHOLDER</title>
 
-        {{-- ICONS --}}
-        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    {{-- Custom CSS --}}
+    <link rel="stylesheet" href="css/global.css">
+    @yield('header')
 
-        {{-- Fonts --}}
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    {{-- ICONS --}}
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
-        {{-- Bootstrap --}}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
-      </head>
+    {{-- Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 
-    <body class="antialiased">
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#"><img src="img/Logo.svg" alt="Logo" height="25"  /></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+</head>
+
+<body class="antialiased">
+
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="img/Logo.svg" alt="Logo" height="25" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="nav-item header-end-point">
-                  <div class="col-10">
+            </button>
+            <div class="nav-item header-end-point">
+                <div class="col-10">
                     <form class="d-flex">
                         <div>
-                            <input class="form-control me-2 rounded-pill navbar-search" type="search" placeholder="PROCURAR..." aria-label="Search">
+                            <input class="form-control me-2 rounded-pill navbar-search" type="search"
+                                placeholder="PROCURAR..." aria-label="Search">
                         </div>
                     </form>
-                  </div>
-                  <div class="col-1">
+                </div>
+                <div class="col-1">
                     <div class="dropdown dropdown-menu-left">
                         <a href="" class="" href="#" role="button" id="dropdownNotify" data-bs-toggle="dropdown" aria-expanded="false">
-                          <ion-icon name="notifications-outline"></ion-icon>
+                            <ion-icon name="notifications-outline"></ion-icon>
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="dropdownNotify">
-                          <li></li>
+                            <li></li>
                         </ul>
-                      </div>
-                  </div>
-                  <div class="col-1">
-                    <img src="img/Logo.svg" class="rounded-circle user-avatar" alt="Business Logo" width="35" height="35">
-                  </div>
-              </div>
-            </div>
-          </nav>
-
-          <main>
-              <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-2 d-sm-none d-md-block sidebar">
-                            <div class="row sb-main-row text-center">
-                              <div class="row first-block">
-                                <div class="col-12">
-                                  <a href="" class="text-white">HOME</a>
-                                </div>
-                              </div>
-                              <div class="row second-block">
-                                <div class="col-12">
-                                  <a href="" class="text-white">CADASTRAR</a>
-                                </div>                                
-                              </div>
-                              <div class="row third-block">
-                                <div class="col-12">
-                                  <a href="" class="text-white">GERENCIAR</a>
-                                </div>
-                              </div>
-                              <div class="row last-block">
-                                <div class="col-12">
-                                  <a href="" class="text-white">RECOLHER</a>
-                                </div>
-                              </div>
-                            </div>
-                    </div>
-                    <div class="col-md-10 main-frame">
-                    @yield('content')
                     </div>
                 </div>
-              </div>
-          </main>
+                <div class="col-1">
+                    <img src="img/Logo.svg" class="rounded-circle user-avatar" alt="Business Logo" width="35" height="35">
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <main>
+        <div class="container-fluid">
+            <div class="row flex-nowrap">
+                <div class="col-auto bg-dark sidebar d-none d-sm-flex p-0">
+                    <div class="d-flex flex-column align-items-sm-start text-white body-content-area">
+                        <ul class="nav nav-pills flex-column mb-sm-auto align-items-sm-start" id="menu">
+                            <li class="sidemenu-item">
+                                <a href="#" class="nav-link align-middle text-white d-flex align-items-center">
+                                    <ion-icon name="home-outline"></ion-icon> <span class="me-1">HOME</span>
+                                </a>
+                            </li>
+                            <li class="sidemenu-item">
+                                <a href="#" class="nav-link align-middle text-white d-flex align-items-center">
+                                    <ion-icon name="create-outline"></ion-icon> <span class="me-1">GERENCIAR</span>
+                                </a>
+                            </li>
+                            <li class="sidemenu-item">
+                                <a href="#" data-bs-toggle="collapse" class="nav-link align-middle text-white d-flex">
+                                    <ion-icon name="add-circle-outline"></ion-icon> <span class="me-1">CADASTRAR</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col py-3 main-frame">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </main>
 
 
-<footer>
+    <footer>
 
-</footer>
+    </footer>
 
-{{-- Custom JavaScript --}}
-<script src="js/global.js"></script>
+    {{-- Custom JavaScript --}}
+    <script src="js/global.js"></script>
 
-{{-- BOOTSTRAP JS --}}
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    {{-- BOOTSTRAP JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
-
-
