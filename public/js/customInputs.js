74d4b654722d38cc83c955b09631f0a77b1d06ac
Heloriel <andradematheus.dev@gmail.com>
2021-11-24 09:03:16 -0300

    Small Optmizations do Custom Inputs JS

--- public/js/customInputs.js
+++ public/js/customInputs.js
@@ -1,30 +1,30 @@
 let input, hashtagArray, container, t;
 
-input = document.querySelector('#hashtags');
-container = document.querySelector('.tag-container');
+input = document.querySelector("#hashtags");
+container = document.querySelector(".tag-container");
 hashtagArray = [];
-hashtags = {}
+hashtags = {};
 
 
-$("#hashtags-container").on('keydown', '#hashtags', function(e) {
-  var keyCode = e.keyCode || e.which;
+$("#hashtags-container").on("keydown", "#hashtags", function(e) {
+  let keyCode = e.keyCode || e.which;
 
   if ((keyCode == 9 || keyCode == 13) && input.value.length > 0) {
     e.preventDefault();
-    var text = document.createTextNode(input.value)
-    var p = document.createElement('p');
+    let text = document.createTextNode(input.value);
+    let p = document.createElement("p");
     container.appendChild(p);
     hashtagArray.push(input.value);
-    var userTags = document.getElementById("userTags");
+    let userTags = document.getElementById("userTags");
     userTags.value = hashtagArray;
     p.appendChild(text);
-    p.classList.add('tag');
-    input.value = '';
+    p.classList.add("tag");
+    input.value = "";
 
-    let deleteTags = document.querySelectorAll('.tag');
+    let deleteTags = document.querySelectorAll(".tag");
 
     for(let i = 0; i < deleteTags.length; i++) {
-      deleteTags[i].addEventListener('click', () => {
+      deleteTags[i].addEventListener("click", () => {
         container.removeChild(deleteTags[i]);
       });
     }
@@ -32,11 +32,11 @@ $("#hashtags-container").on('keydown', '#hashtags', function(e) {
 });
 
 $(document).ready(function(){
-  $('#timepicker').timepicker({
-    timeFormat: 'HH:mm'
+  $("#timepicker").timepicker({
+    timeFormat: "HH:mm"
   });
 
-  $( "#datepicker" ).datepicker({ dateFormat: 'dd/mm/yy' });
+  $( "#datepicker" ).datepicker({ dateFormat: "dd/mm/yy" });
 
-  $( "#pubPicker" ).datepicker({ dateFormat: 'dd/mm/yy' });
+  $( "#pubPicker" ).datepicker({ dateFormat: "dd/mm/yy" });
 });
