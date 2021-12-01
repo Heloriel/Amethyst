86b6378b40a92d9f7cec4fbabcf1e88883ebec13
Heloriel Thrayn <helore.art@gmail.com>
2021-11-30 21:41:05 -0300

    Status Config Delete Update (Bugged)

--- public/js/configStatus.js
+++ public/js/configStatus.js
@@ -5,8 +5,20 @@ function updateStatus(element){
     let id = element.id;
     let property = element.name;
     let value = element.value;
-    id = id.split("-").pop();
-    newStatus[id - 1][property] = value;
+    id = (id.split("-").pop()) - 1;
+    newStatus[id][property] = value;
+}
+
+function deleteStatus(element) {
+    let elementDiv = element.parentElement;
+    elementDiv.classList.remove('d-flex');
+    elementDiv.classList.add('d-none');
+
+    let id = element.id;
+    id = (id.split("-").pop()) - 1;
+
+    newStatus.splice(index, 1);
+    console.log(newStatus);
 }
 
 $("input").change(function(){
