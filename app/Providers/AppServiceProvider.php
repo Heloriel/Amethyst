cb354ea0d4fbd62d8e2601d57c1c0d0183a296e0
Heloriel <andradematheus.dev@gmail.com>
2022-02-01 09:57:29 -0300

    Fix migrate and PHP version issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
 use App\Models\Rank;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       //
+        Schema::defaultStringLength(191);
     }
 }
